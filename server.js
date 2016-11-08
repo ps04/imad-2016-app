f3a892ae264d9a0f584da3722bc944128870d977
ps04 <ps959@snu.edu.in>
2016-11-08 20:45:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/photo.png', function (req, res) {
+app.get('./ui/photo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'photo.png'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
