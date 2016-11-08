6de2ce872830797d9bb66e7a41e17a02f5ae72f2
ps04 <ps959@snu.edu.in>
2016-11-08 20:41:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
+app.get('/ui/photo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'photo.png'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
