e05e72a1fa3bc2cd662897fcbfb683ab5f792db9
ps04 <ps959@snu.edu.in>
2016-11-26 23:06:38 +0530

    Update and rename main.js to script.js

---
+++ ui/script.js
@@ -0,0 +1,9 @@
+console.log('Loaded!');
+$(document).ready(function() {
+	$("ul#nav li:nth-child(1)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(2)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(3)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(4)").css("margin-right", "60px");
+    $("ul#nav li:nth-child(5)").css("margin-right", "0px");
+});
+Contact GitHub API Training Shop Blog About
