7a15a1905611123697abe80516067e20e9bfedab
ps04 <ps959@snu.edu.in>
2016-11-26 23:51:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+/*app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+});*/
 
-app.get('/ui/photo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));
+/*app.get('/ui/photo.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'photo.jpg'));*/
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
