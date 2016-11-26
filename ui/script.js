050ed6c1b9420836549ecd244c08398e6305852d
ps04 <ps959@snu.edu.in>
2016-11-26 23:01:04 +0530

    Add files via upload

---
+++ ui/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("ul#nav li:nth-child(1)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(2)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(3)").css("margin-right", "60px");
+	$("ul#nav li:nth-child(4)").css("margin-right", "60px");
+    $("ul#nav li:nth-child(5)").css("margin-right", "0px");
+});
